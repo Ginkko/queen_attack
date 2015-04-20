d45888b5755b48cba40dd38c54c7f5e5958f64bf
James Robert Williams & Alec Arme <student@epicodus.com>
2015-04-20 16:35:01 -0700

    Satisfy third spec behavior.

--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -6,8 +6,13 @@ describe('Array#queen_attack?') do
   it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
     expect([1,1].queen_attack?([2, 3])).to(eq(false))
   end
+#Queen attack will check to see if the players queen is in line with the opponents piece horizontally. If so the queen can attack and will show as true.
   it('is true if the coordinates are horizontally in line with each other') do
    expect([2,5].queen_attack?([2,7])).to(eq(true))
+   end
+#Queen attack will check to see if the players queen is in line with the opponetnts peice veritcally. If so the queen can attack and will show as true.
+   it('is true if the coordiantes are vertically in line with each other') do
+   expect([4,3].queen_attack?([2,3])).to(eq(true))
    end
 
 end

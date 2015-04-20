79b9ec31ea3d8d181da93cde80fe36d5e1c84e9b
James Robert Williams & Alec Arme <student@epicodus.com>
2015-04-20 16:29:27 -0700

    Satisfy second spec behavior

--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -6,4 +6,8 @@ describe('Array#queen_attack?') do
   it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
     expect([1,1].queen_attack?([2, 3])).to(eq(false))
   end
+  it('is true if the coordinates are horizontally in line with each other') do
+   expect([2,5].queen_attack?([2,7])).to(eq(true))
+   end
+
 end

38120cf2b7cd5607d117ee9892a2647a759079df
James Robert Williams & Alec Arme <student@epicodus.com>
2015-04-20 16:16:40 -0700

    add first spec and update Gemfile

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source('https://rubygems.com')
+
+gem('sinatra')
+gem('sinatra-contrib')
+gem('rspec')
+gem('pry')
---
+++ spec/queen_attack_spec.rb
@@ -0,0 +1,9 @@
+require('rspec')
+require('queen_attack')
+require('pry')
+#Queen attack will check to see if a Queen chess peice can attack another peice on the board. If the peice is not horizontally, vertically, or diagonally in line with the queen, the Queen cannot attack.
+describe('Array#queen_attack?') do
+  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
+    expect([1,1].queen_attack?([2, 3])).to(eq(false))
+  end
+end
